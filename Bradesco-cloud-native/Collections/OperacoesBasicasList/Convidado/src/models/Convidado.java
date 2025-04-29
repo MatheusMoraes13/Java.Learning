71d3dc98161377d26c052800fae8a9bed42795ac
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-04-29 20:26:49 -0300

    update 29/04

--- Bradesco-cloud-native/Collections/OperacoesBasicasList/Convidado/src/models/Convidado.java
+++ Bradesco-cloud-native/Collections/OperacoesBasicasList/Convidado/src/models/Convidado.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.util.Objects;
+
 public class Convidado {
     private String nome;
     private int codigoConvite;
@@ -15,6 +17,27 @@ public class Convidado {
 
     public int getCodigoConvite() {
         return codigoConvite;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof Convidado)) return false;
+        Convidado convidado = (Convidado) o;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+    
+    @Override 
+    public int hashCode(){
+        return Objects.hash(getCodigoConvite());
+    }
+
+    @Override
+    public String toString(){
+        return "Convidado{" + 
+                "nome= '" + nome + '\'' + 
+                ", codigoConvite= " + codigoConvite +
+                '}';
     }
     
 }
