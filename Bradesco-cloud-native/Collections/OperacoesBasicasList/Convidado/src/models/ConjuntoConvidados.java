4a8d542be746fcaacc100031d793ce145525b277
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-04-29 20:13:58 -0300

    update 29/04

--- Bradesco-cloud-native/Collections/OperacoesBasicasList/Convidado/src/models/ConjuntoConvidados.java
+++ Bradesco-cloud-native/Collections/OperacoesBasicasList/Convidado/src/models/ConjuntoConvidados.java
@@ -8,12 +8,32 @@ public class ConjuntoConvidados {
     private Set<Convidado> convidadoSet;
 
     public ConjuntoConvidados(){
-        this.convidadosSet = new HashSet<>();
+        this.convidadoSet = new HashSet<>();
     }
 
     public void adicionarConvidado(String nome, int codigoConvite){
-        convidadoSet.add(new Convidado(nome, codigoConvite))
+        convidadoSet.add(new Convidado(nome, codigoConvite));
     }
 
-    
+    public void removerConvidadoPorCodigoConvite(int codigoConvite){
+        Convidado convidadoParaRemover = null;
+
+        for (Convidado c : convidadoSet){
+            if(c.getCodigoConvite() == codigoConvite){
+                convidadoParaRemover = c;
+
+                break;
+            }
+        }
+
+        convidadoSet.remove(convidadoParaRemover);
+    }
+
+    public int contarConvidados(){
+        return convidadoSet.size();
+    }
+
+    public void exibirConvidados(){
+        System.out.println(convidadoSet);
+    }
 }
