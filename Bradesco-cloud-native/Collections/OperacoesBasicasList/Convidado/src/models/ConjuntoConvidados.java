4364f5ec9947b022ca42a9e3843f743389c8c569
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-04-29 19:56:09 -0300

    update 29/04

---
+++ Bradesco-cloud-native/Collections/OperacoesBasicasList/Convidado/src/models/ConjuntoConvidados.java
@@ -0,0 +1,19 @@
+package models;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoConvidados {
+
+    private Set<Convidado> convidadoSet;
+
+    public ConjuntoConvidados(){
+        this.convidadosSet = new HashSet<>();
+    }
+
+    public void adicionarConvidado(String nome, int codigoConvite){
+        convidadoSet.add(new Convidado(nome, codigoConvite))
+    }
+
+    
+}
