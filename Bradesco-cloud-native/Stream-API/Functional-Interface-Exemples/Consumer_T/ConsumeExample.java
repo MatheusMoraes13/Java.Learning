87fe30f17ac58bf5aafa3860d1bb467030b57634
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-02 02:56:13 -0300

    update 02/05 consumerExample

---
+++ Bradesco-cloud-native/Stream-API/Functional-Interface-Exemples/Consumer_T/ConsumeExample.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+/*Representa uma operação que aceita um argumento do tipo T e não retorna resultado.
+ * É utilizada principalmente para realizar ações, ou efeitos colaterais nos elementos do Stream sem modificar, ou retornar um valor.
+ */
+
+public class ConsumeExample {
+    public static void main(String[] args) {
+
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Usar o Consumer com expressão lambda para imprimir números pares
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0){
+                System.out.println(numero);
+            }
+        };
+
+        numeros.stream().forEach(imprimirNumeroPar);
+     }
+}
