1001f16ea662630250f15f742f4a1c71c7808b96
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-04 05:19:16 -0300

    update 04/05

--- Bradesco-cloud-native/Stream-API/Functional-Interface-Exemples/Predicate/PredicateExample.java
+++ Bradesco-cloud-native/Stream-API/Functional-Interface-Exemples/Predicate/PredicateExample.java
@@ -5,21 +5,20 @@
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
 
-public class PredicateExamle {
+public class PredicateExample {
     public static void main(String[] args) {
 
         // Criar uma lista de palavras
         List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
         // Criar um predicate que verifica se a palavra tem mais de 5 caracteres
-        Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+        //Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
 
         // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
         // imprimir cada palavra que passou no filtro
         palavras.stream()
-                .filter(maisDeCincoCaracteres)
+                .filter(p -> p.length() > 5)
                 .forEach(System.out :: println);
 
 
