8aa040844005ab142094f72f0717a94397f85c5a
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-06 08:57:09 -0300

    update 06/05 api consumer

--- api_consumer/src/main/java/org/aplicacao/Main.java
+++ api_consumer/src/main/java/org/aplicacao/Main.java
@@ -1,17 +1,24 @@
 package org.aplicacao;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.aplicacao.service.APIservice;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        APIservice apiservice = new APIservice();
+        Scanner scanner = new Scanner(System.in);
+
+        String cep;
+        System.out.println("Insira o CEP o qual deseja consultar: ");
+        cep = scanner.nextLine();
+
+        if (cep.length() > 8){
+            System.out.println("CEP inválido");
         }
+
+        System.out.println(apiservice.getAddres(cep));
+        System.out.println(apiservice.getAddres("68350393"));
+
     }
 }
---
+++ api_consumer/src/main/java/org/aplicacao/dto/AddressDTO.java
@@ -0,0 +1,77 @@
+package org.aplicacao.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class AddressDTO {
+
+    private String cep;
+    private String logradouro;
+    private String complemento;
+    private String bairro;
+    private String localidade;
+    private String uf;
+    private String ibge;
+    private String gia;
+    private String ddd;
+    private String siafi;
+
+    public AddressDTO(){
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getLocalidade() {
+        return localidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public String getIbge() {
+        return ibge;
+    }
+
+    public String getGia() {
+        return gia;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public String getSiafi() {
+        return siafi;
+    }
+
+    @Override
+    public String toString() {
+        return "AddressDTO{" +
+                "cep='" + cep + '\'' +
+                ", logradouro='" + logradouro + '\'' +
+                ", complemento='" + complemento + '\'' +
+                ", bairro='" + bairro + '\'' +
+                ", localidade='" + localidade + '\'' +
+                ", uf='" + uf + '\'' +
+                ", ibge='" + ibge + '\'' +
+                ", gia='" + gia + '\'' +
+                ", ddd=" + ddd +
+                ", siafi='" + siafi + '\'' +
+                '}';
+    }
+}
