da0b6530c0b5b3c5a9752f08f988ac32cac77e26
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-06 01:33:01 -0300

    update 06/05 ConsumerExample

--- Bradesco-cloud-native/Stream-API/Functional-Interface-Exemples/Consumer_T/ConsumeExample.java
+++ Bradesco-cloud-native/Stream-API/Functional-Interface-Exemples/Consumer_T/ConsumeExample.java
@@ -6,15 +6,15 @@ import java.util.List;
  */
 
 public class ConsumeExample {
+    
     public static void main(String[] args) {
 
         // Criar uma lista de números inteiros
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         // Usar o Consumer com expressão lambda para imprimir números pares
-       
 
         numeros.stream().filter (n -> n % 2 == 0)
         .forEach(System.out :: println);
-     }
+    }
 }
