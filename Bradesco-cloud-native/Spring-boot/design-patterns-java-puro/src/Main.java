9d7917078b559c057f254ddee2847a6e92b5a555
MatheusMoraes13 <profissional.moraes2004@gmail.com>
2025-05-19 15:58:57 -0300

    Update 19/05 - Projeto de API para consumo VIACEP

--- Bradesco-cloud-native/Spring-boot/design-patterns-java-puro/src/Main.java
+++ Bradesco-cloud-native/Spring-boot/design-patterns-java-puro/src/Main.java
@@ -1,7 +1,53 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import facade.Facade;
+import singleton.SingletonEager;
+import singleton.SingletonLazy;
+import singleton.SingletonLazyHolder;
+import strategy.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Java Project");
+        //singletonTest();
+
+        //strategyTest();
+
+        facadeTest();
+    }
+
+    public static void singletonTest(){
+        SingletonLazy lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+        lazy = SingletonLazy.getInstancia();
+        System.out.println(lazy);
+
+        SingletonEager eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+        eager = SingletonEager.getInstancia();
+        System.out.println(eager);
+
+        SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+        lazyHolder = SingletonLazyHolder.getInstancia();
+        System.out.println(lazyHolder);
+    }
+
+    public static void strategyTest(){
+        Comportamento normal = new ComportamentoNormal();
+        Comportamento defensivo = new ComportamentoDefensivo();
+        Comportamento ofensivo = new ComportamentoOfensivo();
+
+        Robo robo = new Robo();
+        robo.setStrategy(normal);
+        robo.mover();
+
+        robo.setStrategy(defensivo);
+        robo.mover();
+
+        robo.setStrategy(ofensivo);
+        robo.mover();
+    }
+
+    public static void facadeTest(){
+        Facade facade = new Facade();
+        facade.migrarCliente("Matheus", "123123123");
     }
 }
