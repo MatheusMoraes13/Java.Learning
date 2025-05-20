ed5b34efb2cc5de7f48eded377614b3e4c922188
Matheus Moraes <profissional.moraes2004@gmail.com>
2025-05-20 04:00:30 -0300

    Update 20/05 fixed projet

--- Bradesco-cloud-native/Spring-boot/lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
+++ Bradesco-cloud-native/Spring-boot/lab-padroes-projeto-spring/src/main/java/dio/lab_padroes_projeto_spring/LabPadroesProjetoSpringApplication.java
@@ -1,18 +1,15 @@
 package dio.lab_padroes_projeto_spring;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableFeignClients
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "Via cep consumer", version = "1.0", description = "API para realização do consumo da API viacep"))
 public class LabPadroesProjetoSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+		SpringApplication.run(dio.lab_padroes_projeto_spring.LabPadroesProjetoSpringApplication.class, args);
 	}
 
 }
